e4412feae56fe0509ff5ab9c36b648955d8c2d7c
rkmauryag <rakeshkrmaurya12@gmail.com>
2023-01-25 22:41:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Ecomm Project</h1>
+    <button>Normal Button</button>
+    <Button>Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
